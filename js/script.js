23441b4d675010f85f534fca467b102c87cf1053
Alastor2189 <g.lucafarci89@gmail.com>
2022-04-11 10:26:36 +0200

    inserimento commenti JS

--- js/script.js
+++ js/script.js
@@ -3,18 +3,23 @@ console.log(btnGenerates);
 btnGenerates.addEventListener("click",
     function() {
         //Prelievo degli input dall'utente
+        // prelievo nominativo
         const userName = document.getElementById("name-client").value;
 
+        // prelievo numero km da percorrere
         const numKm = document.getElementById("km-ticket").value;
 
         const priceKm = 0.21;
         let ticketPrice = priceKm * numKm;
 
+        // prelievo tipologia biglietto
         const ageUser = document.getElementById("age-client").value;
 
+        // generazione nominativo
         const nameResponse = document.querySelector(".name-surname .bottom")
         nameResponse.innerHTML = `${userName}`;
 
+        // generazione tipologia biglietto
         let offerta;
         switch (ageUser) {
             case 'minorenne':
@@ -34,18 +39,21 @@ btnGenerates.addEventListener("click",
         console.log(offerResponse);
         offerResponse.innerHTML = `${offerta}`;
 
+        // generazione numero carrozza
         let carriage = Math.floor(Math.random() * 10) + 1;
         console.log(carriage);
 
         const numCarriage = document.querySelector(".carriage .bottom")
         numCarriage.innerHTML = `${carriage}`;
 
+        // generazione ID ticket
         let codTicket = Math.floor(Math.random() * (9999 - 9000)) + 9000;
         console.log(codTicket);
 
         const idTicket = document.querySelector(".id .bottom")
         idTicket.innerHTML = `${codTicket}`;
 
+        // generazione prezzo finale
         let totalPrice
         if (ageUser == 'minorenne') {
             totalPrice = ticketPrice - (ticketPrice * 0.20);
@@ -58,11 +66,13 @@ btnGenerates.addEventListener("click",
         const finalPrice = document.querySelector(".ticket-price .bottom")
         finalPrice.innerHTML = ' € ' + (totalPrice.toFixed(2)).replace('.', ',');
 
+
         const detailPassenger = document.querySelector(".detail-passenger");
         detailPassenger.classList.remove("d-none");
     }
 );
 
+// attivazione tasto annulla
 const clearBtn = document.getElementById("cancel");
 
 clearBtn.addEventListener("click",
